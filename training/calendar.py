e13f956f408a8a30e12425dd1f9d51a43f86c3e1
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2025-09-02 10:19:20 +0200

    fix calender training

--- training/calendar.py
+++ training/calendar.py
@@ -7,12 +7,12 @@ while first_day < 1 or first_day > 7:
 while count_days < 28 or count_days > 31:
     count_days = int(input("Count of days in the month (28-31): "))
 
-first_row = [str(x) for x in range(first_day, first_day + (7 - first_day) + 1)]
+first_row = [str(x) for x in range(1, 7 - first_day + 2)]
 print("må  ti  on  to  fr  lö  sö")
 print(f"{" " * 4 * (first_day - 1)}{'   '.join(first_row)}")
 
 rows_count = int((count_days + first_day - 1) / 7)
 for i in range(1, rows_count + 1):
     last_index = 7 if i != rows_count else (count_days + first_day - 1) % 7
-    numbers = [str(7 * i + x) for x in range(1, last_index + 1)]
-    print(f'{'   '.join(numbers)}')
+    numbers = [str(7 * i + x - first_day + 1) for x in range(1, last_index + 1)]
+    print(f"{'   '.join(numbers)}")
